c0a8c2f2f02566691de413986488f2af5703b76f
Ryan <dragonguard7@gmail.com>
2017-03-24 22:00:13 -0400

    Moved website to its own git repository

--- website/portfolio_projects/Carousel/script.js
+++
@@ -1,3 +0,0 @@
-// Code goes here
-
-var app = angular.module('myApp', ['ngMaterial', 'jkAngularCarousel'])
--- website/portfolio_projects/Reader/BookshelfController.js
+++
@@ -1,5 +0,0 @@
-app.controller('BookshelfController', ['$scope', 'books', function($scope, books) {
-  books.getData().then(function(data) {
-    $scope.myBooks = data;
-  });
-}]);
